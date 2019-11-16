094effd7e181dc9039baebc33b48e77865d0301a
Dav1dMwang1 <david.mwangi.john@gmail.com>
2019-11-16 18:25:13 +0300

    Side Navigation Layout
    Adds a side navigation bar.

--- app/src/main/java/com/example/dijonkariz/fotomwa/activity/LandingPageActivity.java
+++ app/src/main/java/com/example/dijonkariz/fotomwa/activity/LandingPageActivity.java
@@ -1,11 +1,15 @@
 package com.example.dijonkariz.fotomwa.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -34,13 +38,33 @@ import java.util.Objects;
 
 public class LandingPageActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private View headerLayout;
+    private ImageView ivHeaderPhoto;
+
+    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView = findViewById(R.id.nav_view);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -62,5 +86,104 @@ public class LandingPageActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        drawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+//        switch (item.getItemId()) {
+//            case R.id.nav_home:
+//                drawerLayout.openDrawer(GravityCompat.START);
+//                return true;
+////            case R.id.nav_orders:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+////            case R.id.nav_photos:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+////            case R.id.nav_notifications:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+////            case R.id.nav_settings:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+////            case R.id.nav_about_us:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+////            case R.id.nav_privacy_policy:
+////                drawerLayout.openDrawer(GravityCompat.START);
+////                return true;
+//
+//        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem (MenuItem menuItem) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                fragmentClass = HomeFragment.class;
+                break;
+            case R.id.nav_orders:
+                fragmentClass = OrdersFragment.class;
+                break;
+            case R.id.nav_photos:
+                fragmentClass = PhotosFragment.class;
+                break;
+            case R.id.nav_notifications:
+                fragmentClass = NotificationsFragment.class;
+                break;
+            case R.id.nav_settings:
+                fragmentClass = SettingsFragment.class;
+                break;
+            case R.id.nav_about_us:
+                fragmentClass = AboutUsActivity.class;
+                break;
+            case R.id.nav_privacy_policy:
+                fragmentClass = PrivacyPolicyActivity.class;
+                break;
+            default:
+                fragmentClass = HomeFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame, fragment).commit();
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+    }
 }
